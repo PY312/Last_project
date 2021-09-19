4d8cf9e759ac5b159ea0f9370e8d596e63a94501
PY312 <83508520+PY312@users.noreply.github.com>
2021-09-19 22:46:34 +0600

    last project

---
+++ Laws/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from Laws.models import *
+
+admin.site.register(LegisFavourite)
+admin.site.register(LawAllNKO)
+admin.site.register(NCOCategory)
